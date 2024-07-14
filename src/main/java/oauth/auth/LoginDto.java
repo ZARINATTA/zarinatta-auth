eeb82fa52f6de016f87e86131938ed3a0d47dcd1
seokyung <sk000801@naver.com>
2024-07-14 23:06:21 +0900

    feat : 응답을 위한 DTO 생성

---
+++ src/main/java/oauth/auth/LoginDto.java
@@ -0,0 +1,15 @@
+package oauth.auth;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class LoginDto {
+    String token_type;
+    String access_token;
+    int expires_in;
+    String refresh_token;
+    int refresh_token_expires_in;
+    String scope;
+}
---
+++ src/main/java/oauth/auth/TestDto.java
@@ -0,0 +1,10 @@
+package oauth.auth;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class TestDto {
+    String message;
+}
