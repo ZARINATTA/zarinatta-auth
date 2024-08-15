41feb2e5e1b25d8bcc6e96e0bae26347abefa71f
JunHyuk Yu <ericyoo0107@gmail.com>
2024-08-16 01:01:23 +0900

    feat: 크롤링 서버에 있던 엔티티, Enum 이동

---
+++ src/main/java/server/entity/User.java
@@ -0,0 +1,20 @@
+package server.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column
+    private String phoneNumber;
+}
