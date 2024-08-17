46eb589823b64ed0f37e52ab03ac435d8afc10c8
JunHyuk Yu <ericyoo0107@gmail.com>
2024-08-18 02:04:49 +0900

    test: 티켓 검색 API 서비스단 테스트

--- src/main/java/server/ticket/controller/dto/request/TicketSearchRequest.java
+++ src/main/java/server/ticket/controller/dto/request/TicketSearchRequest.java
@@ -3,6 +3,7 @@ package server.ticket.controller.dto.request;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 import server.enums.StationCode;
 
@@ -18,4 +19,13 @@ public class TicketSearchRequest {
     private String departDate;
     private String departTime;
     private String trainType;
+
+    @Builder
+    public TicketSearchRequest(StationCode departStation, StationCode arriveStation, String departDate, String departTime, String trainType) {
+        this.departStation = departStation;
+        this.arriveStation = arriveStation;
+        this.departDate = departDate;
+        this.departTime = departTime;
+        this.trainType = trainType;
+    }
 }
